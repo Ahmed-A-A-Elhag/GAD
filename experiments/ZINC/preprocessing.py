36b0a8e999751559515c4574394a460b3ef6d60d
Ahmed A. A. Elhag <38051706+Ahmed-A-A-Elhag@users.noreply.github.com>
2022-04-27 17:27:08 +0000

    Update and rename preprocessing_dataset.py to preprocessing.py

--- experiments/ZINC/preprocessing.py
+++ experiments/ZINC/preprocessing.py
@@ -118,3 +118,25 @@ def preprocessing_dataset(dataset):
         data_list.append(Data.from_dict(data_dic[i]))
     
     return data_list
+
+
+def avg_d(dataset):
+  
+  D = []
+  
+  for i in range(len(dataset)):
+
+      adj = to_dense_adj(dataset[i].edge_index)[0]
+
+      deg = adj.sum(axis=1, keepdim=True) # Degree of nodes, shape [N, 1]
+
+      D.append(deg.squeeze())
+
+  D = torch.cat(D, dim = 0)
+
+  avg_d  = dict(lin=torch.mean(D),
+          exp=torch.mean(torch.exp(torch.div(1, D)) - 1),
+          log=torch.mean(torch.log(D + 1)))
+
+
+  return D, avg_d

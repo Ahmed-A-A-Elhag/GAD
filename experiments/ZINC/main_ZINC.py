2021e8bc2bb65423480f265fe42b395f51d941db
Ahmed A. A. Elhag <38051706+Ahmed-A-A-Elhag@users.noreply.github.com>
2022-04-27 18:24:11 +0000

    Update main_ZINC.py

--- experiments/ZINC/main_ZINC.py
+++ experiments/ZINC/main_ZINC.py
@@ -1,160 +1,112 @@
 import torch
+import torch.optim as opt
 import torch.nn as nn
+
+from torch_geometric.datasets import ZINC
+from torch_geometric.data import DataLoader
+import os
+import sys
 
 from tqdm import tqdm
 
-from torch_geometric.utils import to_dense_adj, get_laplacian
+import argparse
 
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../src/")) 
 
-def evaluate_network(model, data_loader, device):
+from preprocessing import preprocessing_dataset, average_node_degree
+from evaluate import train_epoch, evaluate_network
+from GAD import GAD
 
-    model.eval()
-    
-    epoch_test_mae = 0
-
-
-    loss_fn = nn.L1Loss()
-
-    with torch.no_grad():
-
-        for idx, batched_graph in tqdm(enumerate(data_loader)):
-
-            
-            
-            num_nodes = batched_graph.num_nodes
-            adj = to_dense_adj(batched_graph.edge_index, max_num_nodes = num_nodes)[0].to(device)
-            node_deg_vec = adj.sum(axis=1, keepdim=True).to(device)
-            
-            node_deg_mat = torch.diag(node_deg_vec[:, 0]).to(device)
-
-            lap_mat_sparse = get_laplacian(batched_graph.edge_index)
-            lap_mat = to_dense_adj(edge_index = lap_mat_sparse[0], edge_attr = lap_mat_sparse[1], max_num_nodes = num_nodes)[0].to(device)
-   
-            F_norm_edge = batched_graph.F_norm_edge.to(device)
-            F_dig = batched_graph.F_dig.to(device)
-            
-            edge_index = batched_graph.edge_index.to(device)
-            batch_idx = batched_graph.batch.to(device)
-
-            node_fts = batched_graph.x.to(device)
-            node_fts = node_fts.squeeze()
-            
-            edge_fts = batched_graph.edge_attr.to(device)
-            norm_n = batched_graph.norm_n.to(device)
-            
-            k_eig_val = batched_graph.k_eig_val.to(device)
-            k_eig_vec = batched_graph.k_eig_vec.to(device)
-     
-            out_model = model(node_fts, edge_fts, edge_index, F_norm_edge, F_dig, node_deg_vec, node_deg_mat, lap_mat, k_eig_val, k_eig_vec, num_nodes, norm_n, batch_idx)
-
-            loss = loss_fn(out_model, batched_graph.y.to(device))
-
-            epoch_test_mae += loss.item()  # MAE: mean absolute error
-
-
-        epoch_test_mae /= (idx + 1)
-        
-    return epoch_test_mae
-
-    
-def train_epoch(model ,data_loader, optimizer, device, loss_fn):
-    
-        epoch_train_mae = 0
-
-        model.train() 
-
-        for idx, batched_graph in tqdm(enumerate(data_loader)):
-
-            optimizer.zero_grad()
-        
-            num_nodes = batched_graph.num_nodes
-            
-            adj = to_dense_adj(batched_graph.edge_index, max_num_nodes = num_nodes)[0].to(device)
-            node_deg_vec = adj.sum(axis=1, keepdim=True).to(device)
-            
-            node_deg_mat = torch.diag(node_deg_vec[:, 0]).to(device)
-
-            
-            lap_mat_sparse = get_laplacian(batched_graph.edge_index)
-            lap_mat = to_dense_adj(edge_index = lap_mat_sparse[0], edge_attr = lap_mat_sparse[1], max_num_nodes = num_nodes)[0].to(device)
-
-        
-            F_norm_edge = batched_graph.F_norm_edge.to(device)
-            F_dig = batched_graph.F_dig.to(device)
-     
-            edge_index = batched_graph.edge_index.to(device)
-            batch_idx = batched_graph.batch.to(device)
-
-            node_fts = batched_graph.x.to(device)
-            node_fts = node_fts.squeeze()
-            
-            edge_fts = batched_graph.edge_attr.to(device)
-            
-            norm_n = batched_graph.norm_n.to(device)
-            
-            k_eig_val = batched_graph.k_eig_val.to(device)
-            k_eig_vec = batched_graph.k_eig_vec.to(device)
-
-            out_model = model(node_fts, edge_fts, edge_index, F_norm_edge, F_dig, node_deg_vec, node_deg_mat, lap_mat, k_eig_val, k_eig_vec, num_nodes, norm_n, batch_idx)
-
-            loss = loss_fn(out_model, batched_graph.y.to(device))
-            
-            loss.backward()
-            optimizer.step()
-
-            epoch_train_mae += loss.item() 
-
-        epoch_train_mae /= (idx + 1)
-        return epoch_train_mae, optimizer
-
-
-
-def evaluate_network(model, data_loader, device):
-
-    model.eval()
-    
-    epoch_test_mae = 0
-
+def train_ZINC(model, optimizer, train_loader, val_loader, device, num_epochs):
 
     loss_fn = nn.L1Loss()
 
-    with torch.no_grad():
+    # torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, threshold=0.0001, threshold_mode='rel', cooldown=0, min_lr=0, eps=1e-08, verbose=False)
 
-        for idx, batched_graph in tqdm(enumerate(data_loader)):
+    scheduler = opt.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',
+                                                     factor=0.5, 
+                                                     patience=15,
+                                                   threshold=0.004,
+                                                   verbose=True)
 
-            
-            
-            num_nodes = batched_graph.num_nodes
-            adj = to_dense_adj(batched_graph.edge_index, max_num_nodes = num_nodes)[0].to(device)
-            node_deg_vec = adj.sum(axis=1, keepdim=True).to(device)
-            
-            node_deg_mat = torch.diag(node_deg_vec[:, 0]).to(device)
+    epoch_train_MAEs, epoch_val_MAEs = [], []
+    
+    Best_val_mae = 10
 
-            lap_mat_sparse = get_laplacian(batched_graph.edge_index)
-            lap_mat = to_dense_adj(edge_index = lap_mat_sparse[0], edge_attr = lap_mat_sparse[1], max_num_nodes = num_nodes)[0].to(device)
-   
-            F_norm_edge = batched_graph.F_norm_edge.to(device)
-            F_dig = batched_graph.F_dig.to(device)
-            
-            edge_index = batched_graph.edge_index.to(device)
-            batch_idx = batched_graph.batch.to(device)
-
-            node_fts = batched_graph.x.to(device)
-            node_fts = node_fts.squeeze()
-            
-            edge_fts = batched_graph.edge_attr.to(device)
-            norm_n = batched_graph.norm_n.to(device)
-            
-            k_eig_val = batched_graph.k_eig_val.to(device)
-            k_eig_vec = batched_graph.k_eig_vec.to(device)
-     
-            out_model = model(node_fts, edge_fts, edge_index, F_norm_edge, F_dig, node_deg_vec, node_deg_mat, lap_mat, k_eig_val, k_eig_vec, num_nodes, norm_n, batch_idx)
-
-            loss = loss_fn(out_model, batched_graph.y.to(device))
-
-            epoch_test_mae += loss.item()  # MAE: mean absolute error
-
-
-        epoch_test_mae /= (idx + 1)
+    for epoch in range(num_epochs):
         
-    return epoch_test_mae
+        if optimizer.param_groups[0]['lr'] < 1e-5:
+            print("\n!! LR EQUAL TO MIN LR SET.")
+            break
+        
+        epoch_train_mae, optimizer = train_epoch(model ,train_loader, optimizer, device, loss_fn)
+        epoch_val_mae = evaluate_network(model,  val_loader, device)
+
+        epoch_train_MAEs.append(epoch_train_mae)
+        epoch_val_MAEs.append(epoch_val_mae)
+
+        scheduler.step(epoch_val_mae)
+        if(epoch_val_mae < Best_val_mae):
+            Best_val_mae =  epoch_val_mae
+            torch.save(model, 'model.pth')
+
+        torch.save(model, 'model_running.pth')
+
+        print("")
+        print("epoch_idx", epoch)
+        print("epoch_train_MAEs", epoch_train_mae)
+        print("epoch_val_MAEs", epoch_val_mae)
+        
+        
+def main():
+
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--graph_norm', help="Enter true for graph_norm", type = bool ,default=True)
+    parser.add_argument('--batch_norm', help="Enter true for batch_norm", type = bool ,default=True)
+    parser.add_argument('--L', help="Enter the number of layers", type = int)
+    parser.add_argument('--dropout', help="Enter the rate of the dropout", type = int, default=0)
+    parser.add_argument('--readout', help="Enter the readout agggregator", type = str, default='mean')
+    parser.add_argument('--aggregators', help="Enter the aggregators", type = str)
+    parser.add_argument('--scalers', help="Enter the scalers", type = str)
+    parser.add_argument('--edge_fts', help="Enter true if you want to use edge_fts", type = bool)
+    parser.add_argument('--type_net', help="Enter the type_net", type = str)
+    parser.add_argument('--towers', help="Enter the num of towers", type = int)
+    parser.add_argument('--residual', help="Enter the residual", type = bool)
+    parser.add_argument('--use_diffusion', help="Enter the use_diffusion", type = bool)
+    parser.add_argument('--diffusion_method', help="Enter the diffusion_method", type = str)
+    parser.add_argument('--k', help="Enter the num k", type = int)
+    
+    args = parser.parse_args()
+    
+    dataset_train = ZINC(root='/', subset=True)
+    dataset_val = ZINC(root='/', subset=True, split='val')
+    dataset_test = ZINC(root='/', subset=True, split='test')
+    print("bn")
+
+    D, avg_d = average_node_degree(dataset_train)
+    dataset_train = preprocessing_dataset(dataset_train, 30)
+    dataset_val = preprocessing_dataset(dataset_val, 30)
+    dataset_test = preprocessing_dataset(dataset_test, 30)
+    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    train_loader = DataLoader(dataset = dataset_train, batch_size=16,shuffle=True) 
+    val_loader = DataLoader(dataset = dataset_val, batch_size=16, shuffle=False)
+    test_loader = DataLoader(dataset =  dataset_test, batch_size=16, shuffle=False)
+    
+    model = GAD(num_atom_type = 28, num_bond_type = 4, hid_dim = 65, graph_norm = True, batch_norm = True, dropout = 0,
+                      readout = 'mean', aggregators = 'mean dir_der max min', scalers = 'identity amplification attenuation', 
+                      edge_fts = True, avg_d = avg_d, D = D, device = device, towers=5, type_net = 'tower', 
+                      residual = True, use_diffusion = True, diffusion_method = 'spectral',k = 30, n_layers = 4)
+    
+    lr= 1e-3
+
+    
+    optimizer = opt.Adam(model.parameters(), lr=lr, weight_decay=3e-6)
+    
+    train_ZINC(model, optimizer, train_loader, val_loader, device, num_epochs = 300)
+    
+    
+main()
+    

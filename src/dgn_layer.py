5780303ab58cc78b09e3743f2964ff1b922a1751
Ahmed A. A. Elhag <38051706+Ahmed-A-A-Elhag@users.noreply.github.com>
2022-04-27 18:02:25 +0000

    Update dgn_layer.py

--- src/dgn_layer.py
+++ src/dgn_layer.py
@@ -1,9 +1,9 @@
 import torch
 import torch.nn as nn
 
-from .aggregators import AGGREGATORS
-from .mlp import MLP
-from .scalers import SCALERS
+from aggregators import AGGREGATORS
+from mlp import MLP
+from scalers import SCALERS
 
 
 ########## DGN Layer Simple ############# 
